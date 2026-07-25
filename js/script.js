a98811bb4941898903701a9b042a7ee04d79e1bb
Ezequiel Senosiain <ezesenosiain@gmail.com>
2026-07-25 16:19:50 -0300

    Sección música completa

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const menuButton = document.querySelector(".menu-button");
+const mobileMenu = document.querySelector(".mobile-menu");
+
+
+menuButton.addEventListener("click", () => {
+
+    mobileMenu.classList.toggle("active");
+
+});
