e93b6558b32b1d17098f515f334bbbc84db40d81
Ezequiel Senosiain <ezesenosiain@gmail.com>
2026-07-28 11:21:28 -0300

    Video en proceso

--- js/script.js
+++ js/script.js
@@ -7,3 +7,34 @@ menuButton.addEventListener("click", () => {
     mobileMenu.classList.toggle("active");
 
 });
+
+const cards = document.querySelectorAll(".video-card");
+
+const modal = document.querySelector(".video-modal");
+
+const player = document.querySelector("#youtubePlayer");
+
+cards.forEach(card=>{
+
+    card.addEventListener("click",()=>{
+
+        const id = card.dataset.video;
+
+        player.src =
+        `https://www.youtube.com/embed/${id}?autoplay=1`;
+
+        modal.classList.add("active");
+
+    });
+
+});
+
+document
+.querySelector(".close-video")
+.addEventListener("click",()=>{
+
+    modal.classList.remove("active");
+
+    player.src="";
+
+});
